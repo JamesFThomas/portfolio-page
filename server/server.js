dddc227b8b7918e5c7ac15c554c7205c46100b52
James F. Thomas <james.fthomas@yahoo.com>
2020-09-29 10:36:36 -0500

    (refactor) cleaned up all code, app will disply app.jsx

--- server/server.js
+++ server/server.js
@@ -4,17 +4,7 @@ const path = require('path');
 const app = express();
 const port = 8080;
 
-// const bodyParser = require('body-parser');
-
-// app.use(bodyParser.json);
-// app.use(bodyParser.urlencoded({extended: true}));
-
 app.use("/", express.static(path.join(__dirname, "../build")))
-
-app.get('/', (req, res) => {
-  console.log('hit the GET route');
-  res.send('Hello World!');
-})
 
 app.listen(port, () => {
   console.log(`I am server I listen only on: PORT ${port}`)
