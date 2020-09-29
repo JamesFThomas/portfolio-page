4d12bb3edbbb3fb3affa1c44d5b87116630c0a02
James F. Thomas <james.fthomas@yahoo.com>
2020-09-29 10:21:11 -0500

    (add) app will display fun component and greeting

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
-const express = require('express');
-const app = express();
 require('dotenv').config();
-const port = process.env.PORT;
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 8080;
+
+// const bodyParser = require('body-parser');
+
+// app.use(bodyParser.json);
+// app.use(bodyParser.urlencoded({extended: true}));
+
+app.use("/", express.static(path.join(__dirname, "../build")))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  console.log('hit the GET route');
+  res.send('Hello World!');
 })
 
 app.listen(port, () => {
